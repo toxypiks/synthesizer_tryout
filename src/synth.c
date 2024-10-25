8f0eb7bea66c4441eead5900ba67e1eca319c572
toxypiks <HannaHinkelbein89@gmx.de>
2024-10-25 15:03:24 +0200

    Drawing a sin wave

--- src/synth.c
+++ src/synth.c
@@ -1,6 +1,35 @@
 #include<stdio.h>
+#include <raylib.h>
+#include <math.h>
+
+typedef struct {
+  float phase;
+} Oscillator;
 
 int main() {
-	printf("Hello World\n");
-	return 0;
+  const int screen_width = 1024;
+  const int screen_height = 768;
+  InitWindow(screen_width, screen_height, "synthesizer_tryout");
+
+  SetTargetFPS(60);
+
+  Oscillator osc = {.phase = 0.0f};
+  float signal[screen_width];
+
+  for (size_t i = 0; i < screen_width; ++i)
+	{
+	  signal[i] = sinf((float)i *0.1f);
+	}
+
+  while(!WindowShouldClose())
+  {
+	BeginDrawing();
+	ClearBackground(BLACK);
+	for (size_t i = 0; i < screen_width; ++i)
+	{
+	  DrawPixel(i, screen_height/2 + (int)(signal[i] * 100), BLUE);
+	}
+	EndDrawing();
+  }
+  CloseWindow();
 }
