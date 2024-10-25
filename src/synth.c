f2ca307417baf370778469578012dd9b3872d26b
toxypiks <HannaHinkelbein89@gmx.de>
2024-10-25 20:53:51 +0200

    Play the sweep sound

--- src/synth.c
+++ src/synth.c
@@ -2,13 +2,16 @@
 #include <raylib.h>
 #include <math.h>
 
+#define SAMPLE_RATE 44100
+#define STREAM_BUFFER_SIZE 1024
+
 typedef struct {
   float phase;
 } Oscillator;
 
 void updateSignal(float* signal, float frequency, float sample_duration)
 {
-   for (size_t t = 0; t < 1024; ++t)
+   for (size_t t = 0; t < STREAM_BUFFER_SIZE; ++t)
 	  {
 	    signal[t] = sinf(2*PI * frequency * sample_duration * (float)t);
 	  }
@@ -20,17 +23,27 @@ int main(void)
   const int screen_height = 768;
   InitWindow(screen_width, screen_height, "synthesizer_tryout");
   SetTargetFPS(60);
+  InitAudioDevice();
 
   Oscillator osc = {.phase = 0.0f};
-  float signal[screen_width];
+  float signal[STREAM_BUFFER_SIZE];
   float frequency = 5.0f;
-  unsigned int sample_rate = 1024;
-  float sample_duration = (1.0f / screen_width);
+  float sample_duration = (1.0f / SAMPLE_RATE);
 
+  SetAudioStreamBufferSizeDefault(STREAM_BUFFER_SIZE);
+  AudioStream synth_stream = LoadAudioStream(SAMPLE_RATE, sizeof(float)*8, 1);
+
+  SetAudioStreamVolume(synth_stream, 0.05f);
+  PlayAudioStream(synth_stream);
   while(!WindowShouldClose())
   {
-	updateSignal(signal, frequency, sample_duration);
-	frequency += 0.01f;
+	if (IsAudioStreamProcessed(synth_stream))
+	{
+	 updateSignal(signal, frequency, sample_duration);
+	 UpdateAudioStream(synth_stream, signal, STREAM_BUFFER_SIZE);
+	 frequency += 0.5f;
+	}
+
 	BeginDrawing();
 	ClearBackground(BLACK);
 	DrawText(TextFormat("Frequency: %f", frequency), 100, 100, 20, RED);
@@ -40,5 +53,7 @@ int main(void)
 	}
 	EndDrawing();
   }
+  UnloadAudioStream(synth_stream);
+  CloseAudioDevice();
   CloseWindow();
 }
