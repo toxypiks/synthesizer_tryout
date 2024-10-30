07b3cb0eba69a35883d0d428f77355029618c3e9
toxypiks <HannaHinkelbein89@gmx.de>
2024-10-30 19:30:46 +0100

    Additive synthesis using multiple oscillators

--- src/synth.c
+++ src/synth.c
@@ -4,6 +4,8 @@
 
 #define SAMPLE_RATE 44100
 #define STREAM_BUFFER_SIZE 1024
+#define NUM_OSCILLATORS 16
+
 
 typedef struct {
   float phase;
@@ -25,6 +27,23 @@ void updateSignal(float* signal, Oscillator* osc)
 	  }
 }
 
+void zeroSignal(float* signal)
+{
+   for (size_t t = 0; t < STREAM_BUFFER_SIZE; ++t)
+	  {
+	    signal[t] = 0.0f;
+	  }
+}
+
+void accumulateSignal(float* signal, Oscillator* osc, float amplitude)
+{
+   for (size_t t = 0; t < STREAM_BUFFER_SIZE; ++t)
+	  {
+		updateOsc(osc);
+	    signal[t] += sinf(2.0f*PI * osc->phase) * amplitude;
+	  }
+}
+
 int main(void)
 {
   const int screen_width = 1024;
@@ -41,21 +60,33 @@ int main(void)
 
   float frequency = 5.0f;
   float sample_duration = (1.0f / SAMPLE_RATE);
-  Oscillator osc = {.phase = 0.0f, .phase_stride = frequency * sample_duration};
+  // Oscillator osc = {.phase = 0.0f, .phase_stride = frequency * sample_duration};
   // LFO: low frequency oscillator
-  Oscillator lfo = {.phase = 0.0f, .phase_stride = 1000.0f * sample_duration};
+  // Oscillator lfo = {.phase = 0.0f, .phase_stride = 1000.0f * sample_duration};
+  Oscillator osc[NUM_OSCILLATORS] = {0};
   float signal[STREAM_BUFFER_SIZE];
+
+  float detune = 0.1f;
 
   while(!WindowShouldClose())
   {
 	Vector2 mouse_pos = GetMousePosition();
+	float normalized_mouse_x = (mouse_pos.x /(float)screen_width);
+	detune = 1.0f + normalized_mouse_x * 10.0f;
 	if (IsAudioStreamProcessed(synth_stream))
 	{
-	    updateOsc(&lfo);
-	    frequency = 220.0f + (mouse_pos.x /(float)screen_width) * 100.0f;
-	    osc.phase_stride = frequency * sample_duration;
-
-	    updateSignal(signal, &osc);
+		zeroSignal(signal);
+		float base_freq = 50.0f + (normalized_mouse_x * 1000.0f);
+		for (size_t i = 0; i < NUM_OSCILLATORS; ++i)
+		{
+		  if (i % 2 != 0)
+		  {
+		    frequency = base_freq * i;
+		    float phase_stride = frequency * sample_duration;
+		    osc[i].phase_stride = phase_stride;
+		    accumulateSignal(signal, &osc[i], 1.0f / NUM_OSCILLATORS);
+		  }
+		}
 	    UpdateAudioStream(synth_stream, signal, STREAM_BUFFER_SIZE);
 	}
 
