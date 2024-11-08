4ac7fd93b7f72bdb4f496ad1ccd8f7c87bdfcadd
toxypiks <HannaHinkelbein89@gmx.de>
2024-11-08 14:11:15 +0100

    Refactor code and use struct for jack stuff to avoid global variables

--- src/additive_synth_with_jack.c
+++ src/additive_synth_with_jack.c
@@ -5,13 +5,15 @@
 #include <unistd.h>
 #include <jack/jack.h>
 
-jack_port_t* outputPort = 0;
-jack_ringbuffer_t *rb = NULL;
-
-typedef struct {
+typedef struct Osc {
   float freq;
   float time_step;
 } Osc;
+
+typedef struct JackStuff {
+  jack_port_t* output_port;
+  jack_ringbuffer_t* ringbuffer;
+} JackStuff;
 
 
 void gen_signal_in_buf(float* buf, size_t buf_length, Osc* osc) {
@@ -21,20 +23,20 @@ void gen_signal_in_buf(float* buf, size_t buf_length, Osc* osc) {
   }
 }
 
-int process(jack_nframes_t nframes, void* )
+int process(jack_nframes_t nframes, void* jack_stuff_raw)
 {
-  float* outputBuffer= (float*)jack_port_get_buffer ( outputPort, nframes);
+  JackStuff* jack_stuff = (JackStuff*)jack_stuff_raw;
+  float* output_buffer= (float*)jack_port_get_buffer (jack_stuff->output_port, nframes);
 
-  // aus dem Ringbuffer daten in out schreiben
-  if(rb){ // gibts den Ringbuffer überhaupt?
-    // wollen nframes an daten haben (? >nframes daten im Buffer)
-	size_t num_bytes = jack_ringbuffer_read_space (rb);
-	if(num_bytes > (nframes* sizeof(float))) {
-	  jack_ringbuffer_read(rb, (char*)outputBuffer, nframes * sizeof(float));
+  if(jack_stuff->ringbuffer){ // is buffer even there?
+    // see if theres enough data in buffer to read nframes out of it (num_bytes >nframes)
+	size_t num_bytes = jack_ringbuffer_read_space (jack_stuff->ringbuffer);
+	if(num_bytes >= (nframes* sizeof(float))) {
+	  jack_ringbuffer_read(jack_stuff->ringbuffer, (char*)output_buffer, nframes * sizeof(float));
 	} else {
 	  for ( int i = 0; i < (int) nframes; i++)
 	  {
-        outputBuffer[i] = 0.0;
+        output_buffer[i] = 0.0;
 	  }
 	}
   }
@@ -44,38 +46,41 @@ int process(jack_nframes_t nframes, void* )
 
 int main(void) {
 
+  JackStuff jack_stuff = {.output_port = 0, .ringbuffer = NULL};
+
   jack_client_t* client = jack_client_open ("JackClientAdditiveSynth",
                                             JackNullOption,
                                             0,
                                             0 );
-  jack_set_process_callback  (client, process , 0);
 
-  outputPort = jack_port_register ( client,
+  jack_stuff.output_port = jack_port_register (client,
                                     "output",
                                     JACK_DEFAULT_AUDIO_TYPE,
                                     JackPortIsOutput,
                                     0 );
-  size_t my_size = 192000 * sizeof(float);
-  rb = jack_ringbuffer_create (my_size);
 
+  size_t my_size = 192000 * sizeof(float);
+  jack_stuff.ringbuffer = jack_ringbuffer_create (my_size);
+
+  jack_set_process_callback(client, process ,(void*)&jack_stuff);
   jack_activate(client);
 
   float my_data_buf[1024];
 
   Osc my_osci = {.freq = 440, .time_step = 0};
 
-  // hier signalerzeugung
-  // nächste mal in eigenen thread
-  // 10 sekunden ausgeben
+  // create signal
+  // has to run in own thread TODO
+  // play 10 sec
   size_t counter = 0;
   while(counter < 48000 * 10 ){
-	//häppchenweise daten in buffer
-	// bei jeden Happpen checken wieviel schon drin
-	// wenn da schon 2sekunden drin sind -> sleep(1)
-	size_t num_bytes = jack_ringbuffer_read_space (rb);
+	// put data chunkwise in buffer
+	// after each chunk ask how much data is in buffer
+	// more then 2 sec of data in buffer -> sleep(1)
+	size_t num_bytes = jack_ringbuffer_read_space (jack_stuff.ringbuffer);
 	if (num_bytes < 96000 * sizeof(float)) {
 	  gen_signal_in_buf(my_data_buf, 1024, &my_osci);
-	  jack_ringbuffer_write (rb, (void *) my_data_buf, 1024*sizeof(float));
+	  jack_ringbuffer_write (jack_stuff.ringbuffer, (void *) my_data_buf, 1024*sizeof(float));
 	  counter += 1024;
 	} else {
 	  sleep(1);
@@ -84,6 +89,6 @@ int main(void) {
 
   jack_deactivate(client);
   jack_client_close(client);
-  jack_ringbuffer_free(rb);
+  jack_ringbuffer_free(jack_stuff.ringbuffer);
   return 0;
 }
